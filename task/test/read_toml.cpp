46e37a81e296844f198241a82833e4bfe6d340e5
Interweave <interweave@qq.com>
2026-04-13 14:54:16 +0000

    fix: 修复项目结构

--- task/test/read_toml.cpp
+++ task/test/read_toml.cpp
@@ -1,27 +1,30 @@
+//
+// Created by Interweave on 2026/4/09.
+//
+
+#include <iomanip>
 #include <iostream>
-#include <toml.hpp>
+
+#include "tools/tomlpp.hpp"
 
 int main() {
+    std::cout << std::setprecision(17);
     auto config = toml::parse_file("../../config/testconfig.toml");
 
     std::string title = config["title"].value_or("default");
 
-    // int id = config["robot"]["id"].value_or(0);
-    // double speed = config["robot"]["speed"].value_or(0.0);
-    //
-    // std::cout << title << std::endl;
-    // std::cout << "id: " << id << std::endl;
-    // std::cout << "speed: " << speed << std::endl;
+    double exposure_ms = config["camera"]["exposure_ms"].value_or(0);
+    double gain = config["camera"]["gain"].value_or(0.0);
+
+    std::cout << title << std::endl;
+    std::cout << "exposure_ms: " << exposure_ms << std::endl;
+    std::cout << "gain: " << gain << std::endl;
 
     auto arr1 = config["camera"]["camera_matrix"].as_array();
-
     double K[3][3];
-
     for (int i = 0; i < 9; i++) {
         K[i / 3][i % 3] = (*arr1)[i].value_or(0.0);
     }
-
-    // 打印
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             std::cout << K[i][j] << " ";
@@ -30,10 +33,6 @@ int main() {
     }
 
     auto arr = config["camera"]["distort_coeffs"].as_array();
-
-    if (!arr || arr->size() != 5) {
-        throw std::runtime_error("distort_coeffs must be size 5");
-    }
 
     for (size_t i = 0; i < 5; i++) {
         double val = (*arr)[i].value_or(0.0);
